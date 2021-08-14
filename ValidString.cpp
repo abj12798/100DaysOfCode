80e017f2be5bf51c3e8e5e588650508e4e7bcf00
AMAN JAIN <amanjain@AMANs-MacBook-Air.local>
2021-08-14 12:18:03 +0530

    valid strings

---
+++ ValidString.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+void valid_string(string out, int *arr, int n, int i, map<int, string> m, vector<string> &r)
+{
+
+    //base-case
+    if (i == n)
+    {
+        r.push_back(out);
+        return;
+    }
+
+    if (i < n)
+    {
+        out =
+    }
+}
