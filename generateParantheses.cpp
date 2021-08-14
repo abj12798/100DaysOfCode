cc39834ab391f2f335b3e9acd8fb70b5c2ff684d
AMAN JAIN <amanjain@AMANs-MacBook-Air.local>
2021-08-14 12:16:50 +0530

    generate parenthesis

--- generateParantheses.cpp
+++ generateParantheses.cpp
@@ -1,49 +1,54 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-void generate_brackets(char* out,int indx,int open,int close,int n){
+void generate_brackets(char *out, int indx, int open, int close, int n)
+{
 
-    if(indx == (2*n)){
+    if (indx == (2 * n))
+    {
         out[indx] = '\0';
-        cout<<out<<endl;
+        cout << out << endl;
         return;
     }
 
-    if(open < n){
+    if (open < n)
+    {
         out[indx] = '(';
-        generate_brackets(out,indx + 1, open + 1, close, n);
+        generate_brackets(out, indx + 1, open + 1, close, n);
     }
 
-    if(close < open){
+    if (close < open)
+    {
         out[indx] = ')';
         generate_brackets(out, indx + 1, open, close + 1, n);
-
     }
     return;
 }
 
-vector<string> generate_brackets(int n){
+vector<string> generate_brackets(int n)
+{
 
     vector<string> result;
     result.clear();
     //generate_brackets("",0,0,0,n);
 
     return result;
-
 }
 
-int main(){
+int main()
+{
 
     int T;
-    cin>>T;
+    cin >> T;
 
-    while(T--){
+    while (T--)
+    {
         int n;
-        cin>>n;
-        char* out = new char[2*n];
-        generate_brackets(out,0,0,0,n);
+        cin >> n;
+        char *out = new char[2 * n];
+        generate_brackets(out, 0, 0, 0, n);
     }
     return 0;
 }
